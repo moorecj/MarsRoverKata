62ab0898dc7262f4d27241a37cf5bee33f0db286
moorecj <moore.cyril@gmail.com>
2014-06-17 15:54:21 -0400

    Initial commit of the Mars Rover Kata

---
+++ MarsRoverTests/MarsRoversTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MarsRoverKata;
+
+
+namespace MarsRoverTests
+{
+    [TestFixture]
+    public class MarsRoversTests
+    {
+        [Test]
+        public void AMarsRoverExists()
+        {
+            MarsRover rover = new MarsRover();
+
+            Assert.That(rover, Is.Not.Null);
+        }
+
+    }
+}
